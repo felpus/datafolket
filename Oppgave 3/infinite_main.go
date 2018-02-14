00899e7530f8bf67f958c1aa35b18225e872e0b5
Rajaa15 <35818294+Rajaa15@users.noreply.github.com>
2018-02-14 20:43:27 +0100

    Flyttet fil

---
+++ Oppgave 3/infinite_main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"./infinite"
+	"fmt"
+	"os"
+	"os/signal"
+)
+func main(){
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, os.Interrupt)
+	go func() {
+		<-sigs
+		fmt.Printf("You pressed ctrl + C. User interrupted infinite loop.")
+		os.Exit(0)
+	}()
+	Loop()
+}
+
+func Loop() {
+	infinite.Infinite01("")
+}
