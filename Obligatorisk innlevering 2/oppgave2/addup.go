74729d5a0d57448544037d2025b866997aacd8d7
Filip Unander <filippunander@gmail.com>
2018-03-09 23:52:44 +0100

    oblig 2

---
+++ Obligatorisk innlevering 2/oppgave2/addup.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	_, err := fmt.Scanf("%d", &i)
+}
+
---
+++ Obligatorisk innlevering 2/oppgave3/addup.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	d := make(chan os.Signal, 1)
+	signal.Notify(d, os.Interrupt)
+	go func() {
+		<-d
+		fmt.Println("Feilmelding")
+		os.Exit(1)
+	}()
+	c := make(chan int)
+	go readInput(c)
+	time.Sleep(5 * 1e9)
+	go addUp(c)
+	time.Sleep(1 * 1e9)
+}
+
+func readInput(c chan int) {
+	var n1 int
+	var n2 int
+	fmt.Println("Skriv verdi: ")
+	fmt.Scan(&n1)
+	fmt.Println("Skriv verdi: ")
+	fmt.Scan(&n2)
+	c <- n1
+	c <- n2
+	res := <-c
+	fmt.Println("Resultat: ", res)
+}
+
+func addUp(c chan int) {
+	n1, n2 := <-c, <-c
+	res := n1 + n2
+	c <- res
+}
