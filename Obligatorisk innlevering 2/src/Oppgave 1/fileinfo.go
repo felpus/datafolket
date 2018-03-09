bfc4b30b0abe404ffed8a50cfb8281ca95ed059e
Rajaa15 <35818294+Rajaa15@users.noreply.github.com>
2018-03-09 23:48:45 +0100

    Update fileinfo.go

--- Obligatorisk innlevering 2/src/Oppgave 1/fileinfo.go
+++ Obligatorisk innlevering 2/src/Oppgave 1/fileinfo.go
@@ -27,26 +27,28 @@ func fileInfo(filnavn string) {
 	giga := mega / 1024
 
 	//Informasjon om filen printes ut via Println
-	fmt.Println("Infomation about file:", filenavn)
+	fmt.Println("Information about file:", filnavn)
 	fmt.Println("Bytes: ", bytes)
 	fmt.Println("Kilobytes: ", kilo)
 	fmt.Println("Megabytes: ", mega)
 	fmt.Println("Gigabytes: ", giga)
 
 
-	//Logikk for å identifisere flere av filens egenskaper
+	//Logikk basert på if-setninger for å identifisere flere av filens egenskaper
 
 	//Hvis filen er en mappe, vil if-setningen vise true, hvis ikke vil den vise false
 	if fileInfo.Mode().IsDir() == true {
 		fmt.Println("Is a directory")
-	} else if fileInfo.Mode().IsDir() == false {
+	} 
+	else if fileInfo.Mode().IsDir() == false {
 		fmt.Println("Is not a directory")
 
 
 		//Hvis filen er en "vanlig" fil, viser if-setningen true, hvis ikke vil den vise false
 		if fileInfo.Mode().IsRegular() {
 			fmt.Println("Is a regular file")
-		} else {
+		} 
+		else {
 			fmt.Println("Is not a regular file")
 		}
 
@@ -56,21 +58,24 @@ func fileInfo(filnavn string) {
 		//Sjekker om filen er "Append only" eller ikke
 		if fileInfo.Mode() & os.ModeAppend == os.ModeAppend {
 			fmt.Println("Is append only")
-		} else {
+		} 
+		else {
 			fmt.Println("Is not append only")
 		}
 
 		//Sjekker om filen er en device file eller ikke
 		if fileInfo.Mode() & os.ModeDevice == os.ModeDevice {
 			fmt.Println("Is a device file")
-		} else {
+		} 
+		else {
 			fmt.Println("Is not a device file")
 		}
 
 		//Viser om filen er en symbolsk link eller ikke
 			if fileInfo.Mode() & os.ModeSymlink == os.ModeSymlink {
 				fmt.Println("Is a symbolic link")
-			} else {
+			} 
+		else {
 				fmt.Println("Is not a Symbolic link")
 			}
 		}
