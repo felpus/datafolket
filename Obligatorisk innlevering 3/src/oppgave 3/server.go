8aba936b9560e7bb5d7511508d9d020c8d48aaa5
Filip Unander <filippunander@gmail.com>
2018-04-29 17:21:22 +0200

    opg3

---
+++ Obligatorisk innlevering 3/src/oppgave 3/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+)
+
+var (
+	quote = "I want to die on Mars, just not on impact."
+)
+func main() {
+	go TCP()
+	go UDP()
+	fmt.Println("Tjener starter")
+}
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println("Feil: ", err)
+		os.Exit(0)
+	}
+}
+func TCP() {
+	ln, err := net.Listen("tcp", ":17")
+	CheckError(err)
+	conn, err := ln.Accept()
+	CheckError(err)
+	defer conn.Close()
+	for {
+		fmt.Printf("Error during typing: %v", err)
+	}
+}
+func UDP() {
+	address, err := net.ResolveUDPAddr("udp", ":17")
+	CheckError(err)
+	connection, err := net.ListenUDP("udp", address)
+	CheckError(err)
+	defer connection.Close()
+
+	p := make([]byte, 128)
+
+	for {
+		_, addr, err := connection.ReadFromUDP(p)
+		connection.WriteToUDP([]byte(quote), addr)
+		CheckError(err)
+	}
+}
